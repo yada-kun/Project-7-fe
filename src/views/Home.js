07d3a5cb491fb9f74fcdd28046cea836d45aff08
James Anuncio <anunciojames14@gmail.com>
2022-07-29 01:35:39 +0800

    Fix: replaced div with main tag

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,16 +1,15 @@
-
 import Banner from "../components/Home Component/Banner";
 import TopTours from "../components/Home Component/TopTours";
 import TourPackages from "../components/Home Component/TourPackages";
 
 function Home() {
   return (
-    <div>
-    <Banner />
-    <TopTours />
-    <TourPackages /> 
-    </div>
-  )
+    <main>
+      <Banner />
+      <TopTours />
+      <TourPackages />
+    </main>
+  );
 }
 
 export default Home;
